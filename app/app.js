ace5e081437448e6f32d9f28938e835615c9906d
Ryan B <ryan.bliton@gmail.com>
2017-01-05 10:17:00 -0600

    add appConstants and appConfig

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 var app = angular.module("QuestApp", ["ngRoute"]) 
-  .constant("firebaseURL","https://abcquest.firebaseio.com/");
 
-app.config(function($routeProvider){
-  $routeProvider.
-  when('/', {
-    templateUrl:'partials/dungeon.html',
-    controller:'DungeonCtrl'
-  }).
-  otherwise('/');
-});
+
+
+//   .constant("firebaseURL","https://abcquest.firebaseio.com/");
+
+// app.config(function($routeProvider){
+//   $routeProvider.
+//   when('/', {
+//     templateUrl:'partials/dungeon.html',
+//     controller:'DungeonCtrl'
+//   }).
+//   otherwise('/');
+// });
---
+++ app/appConfig.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBZrqAIuEZ-USlTFBzmojpFHCBdbb7ITJw",
+    authDomain: "abcquest.firebaseapp.com",
+    databaseURL: "https://abcquest.firebaseio.com",
+    storageBucket: "firebase-abcquest.appspot.com",
+    messagingSenderId: "650717282966"
+  };
+  firebase.initializeApp(config);
